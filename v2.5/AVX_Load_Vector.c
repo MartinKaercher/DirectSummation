6490436c73647bb365f7fd49729bdc03021f7038
Martin3235 <m_kaercher@web.de>
2023-04-16 21:01:56 +0200

    Implement multiplication of two 4-vectors.

--- v2.5/AVX_Load_Vector.c
+++ v2.5/AVX_Load_Vector.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <time.h>
 #include <string.h>
-//#include <immintrin.h>
+#include <immintrin.h>
 
 double vec_mult_3D(double *v1, double *v2){
     double ret_vec;
@@ -26,10 +26,18 @@ int main(int argc, char *argv[]){
     }
 
     // Now initialize the same vector but as a __m256 vector
-    // __m256 AVX_test_vec_1 = _mm_set_ps(1.234, 2.345, 3.456, 4.567);
-    // __m256 AVX_test_vec_2 = _mm_set_ps(1.234, 2.345, 3.456, 4.567);
+    __m256d AVX_test_vec_1 = _mm256_set_pd(1.234, 2.345, 3.456, 4.567);
+    __m256d AVX_test_vec_2 = _mm256_set_pd(1.234, 2.345, 3.456, 4.567);
     //
     // __m256 AVX_mult_result;
-    // AVX_mult_result = _mm256_mul_ps(AVX_test_vec_1, AVX_test_vec_2);
+    __m256d AVX_mult_result = _mm256_mul_pd(AVX_test_vec_1, AVX_test_vec_2);
+    double result[4];
+    double* result_pd =  result;
+    _mm256_store_pd(result_pd, AVX_mult_result);
+
+    for (i=0;i<4;i++)
+    {
+        printf("%f \n", result_pd[i]);
+    }
     return 0;
 }
