d5b523a3b28e4b542c3e9227b0024f9483cafe40
Martin3235 <m_kaercher@web.de>
2023-04-20 21:52:18 +0200

    Implement sum along vector.

--- v2.5/AVX_Load_Vector.c
+++ v2.5/AVX_Load_Vector.c
@@ -19,11 +19,15 @@ int main(int argc, char *argv[]){
 
     int i=0;
     double mult_result;
+    double result_serial = 0;
     for (i=0;i<4;i++)
     {
         mult_result = test_vec_1[i]*test_vec_2[i];
         printf("%f \n", mult_result);
+        result_serial += mult_result;
+
     }
+    printf("Result from serial mult and add %f \n", result_serial);
 
     // Now initialize the same vector but as a __m256 vector
     __m256d AVX_test_vec_1 = _mm256_set_pd(1.234, 2.345, 3.456, 4.567);
@@ -31,13 +35,33 @@ int main(int argc, char *argv[]){
     //
     // __m256 AVX_mult_result;
     __m256d AVX_mult_result = _mm256_mul_pd(AVX_test_vec_1, AVX_test_vec_2);
+
+    // Just an 0ed proxy vector
+    __m256d proxy_vec = _mm256_set_pd(0., 0., 0., 0.);
+
+    //Sum the two vectors to (x1[0]+x1[1], x2[0]+x2[1], x1[2]+x1[3], x2[2]+x2[3])
+    __m256d AVX_sum = _mm256_hadd_pd(AVX_mult_result, proxy_vec);
+    // Extract upper 128 bits
+    __m128d AVX_sum_high = _mm256_extractf128_pd(AVX_sum, 1);
+
+    __m128d AVX_result = _mm_add_pd(AVX_sum_high, _mm256_castpd256_pd128(AVX_sum));
+
+
+
+
     double result[4];
-    double* result_pd =  result;
+    double* result_pd =  &result[0];
     _mm256_store_pd(result_pd, AVX_mult_result);
+
+    double result_sum[2];
+    double* AVX_sum_result = & result_sum[0];
+    _mm_store_pd(AVX_sum_result, AVX_result);
 
     for (i=0;i<4;i++)
     {
         printf("%f \n", result_pd[i]);
     }
+
+    printf("Result from AVX/SSE mult and add: %f \n", AVX_sum_result[0]);
     return 0;
 }
